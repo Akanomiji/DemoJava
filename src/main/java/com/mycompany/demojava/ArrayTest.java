4e8b8482fc7c155643f6eb157e6ea9bccfb95950
Akanomiji <65529266+Akanomiji@users.noreply.github.com>
2024-06-26 10:29:12 +0700

    042

--- src/main/java/com/mycompany/demojava/ArrayTest.java
+++ src/main/java/com/mycompany/demojava/ArrayTest.java
@@ -6,8 +6,8 @@ public class ArrayTest {
 
 
     public static void main(String[] args) {
-        System.out.println(getTextNumber(10));
-        
+        System.out.println(getTextNumber(9));
+        System.out.println(amountNumber(0));
     }
     
     public static String getTextNumber(int number) {
@@ -15,8 +15,18 @@ public class ArrayTest {
         if(number >=1||number<=text.length-1){
             return text[number-1];
         }
-        //return"unknow";
-        else return"unknow";
+        return"unknow";
+        //else return"unknow";
+    }
+    
+    public static int amountNumber(int x) {
+        int[] number={1,2,3,5,2,2,1,1,3,4,2,3,1,5,4,1,2,3,2,1,5,4,1,2,3,2,1,2,2,2,2};
+        int c=0;
+        if (x>=0) {
+            return number[x-1];
+        }
+        System.out.println();
+        return c;
     }
     
 }
